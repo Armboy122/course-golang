8d0a52c0eb570d576772f5a7f22194a3e1dfc514
Armboy122 <arm.09686@gmail.com>
2023-05-10 13:44:26 +0700

    setup db

--- exp/gorm.go
+++ exp/gorm.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type Product struct {
+	ID    uint `gorm:"primaryKey"`
+	Name  string
+	Price float64
+	Stock int
+}
+
+func main() {
+	url := "host=localhost user=peagolang password=supersecret dbname=peagolang port=54329 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.Migrator().AutoMigrate(
+		&Product{},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	shirt := Product{
+		Name:  "T-Shirt",
+		Price: 350,
+		Stock: 200,
+	}
+
+	db.Create(&shirt)
 }
